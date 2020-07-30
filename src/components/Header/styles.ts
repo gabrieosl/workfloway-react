0f9846b436d6e6cba2a1754be5bd958b9d7908f2
gabrieosl <ga.bri.el@msn.com>
2020-07-30 15:08:42 -0300

    feature: header responsive

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,47 +1,90 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
-export const Container = styled.header`
+const appearFromRight = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+`;
+interface ContainerProps {
+  showProfile: boolean;
+}
+
+export const Container = styled.header<ContainerProps>`
   display: flex;
   align-items: center;
   justify-content: space-between;
   height: 50px;
+  margin: 0 20px;
   flex-shrink: 0;
   /* box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.3); */
   border-bottom: 1px solid rgba(0, 0, 0, 0.1);
   z-index: 10;
 
-  @media all and (max-width: 500px) {
-    display: none;
-  }
-
   > strong {
-    margin-left: 20px;
     font-size: 24px;
   }
 
   aside {
+    position: relative;
     display: flex;
-    justify-content: center;
+    justify-content: flex-end;
     align-items: center;
-    margin-right: 15px;
+    transition: all 700ms;
 
-    svg {
-      color: #000;
+    overflow: hidden;
+
+    @media all and (max-width: 500px) {
+      ${props =>
+        !props.showProfile &&
+        css`
+          transform: translateX(calc(100% - 20px));
+        `}
     }
 
     strong {
       font-size: 18px;
-      margin-left: 10px;
-      margin-right: 20px;
       color: #000;
+      margin: 0 10px;
     }
 
     button {
       border: 0;
-      background: #000;
+
+      &#logout {
+        background: #333;
+
+        @media all and (min-width: 501px) {
+          box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
+        }
+      }
+
+      &#profile {
+        background: none;
+        transition: all 2s;
+
+        svg {
+          color: #000;
+          &:last-child {
+            display: none;
+          }
+        }
+
+        @media all and (max-width: 500px) {
+          svg:first-child {
+            display: ${props => (props.showProfile ? 'none' : 'block')};
+          }
+          svg:last-child {
+            display: ${props => (props.showProfile ? 'block' : 'none')};
+            color: #b30000;
+          }
+        }
+      }
+
       padding: 5px;
       border-radius: 6px;
-      box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
 
       svg {
         color: #fff;
